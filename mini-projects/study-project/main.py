1cecf409f24564debde54b8facc467fa16edc686
Vamba45 <vambafool@bk.ru>
2023-05-21 02:33:35 +0300

    get data from page inputs

--- mini-projects/study-project/main.py
+++ mini-projects/study-project/main.py
@@ -1,6 +1,26 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Body
+from fastapi.responses import FileResponse
+import cv2
 
 app = FastAPI()
 
-app.mount("", StaticFiles(directory="./", html=True))
+@app.get("/")
+def root():
+    return FileResponse("public/index.html")
+
+@app.post("/")
+# def hello(name = Body(embed=True)):
+def post(data=Body()):
+    name = data["name"]
+    age = data["age"]
+    photo = data["photo"]
+
+    #код для обработки модели
+
+    return {"message": f"{name}, ваш возраст - {age}, фото - {photo}"}
+
+def predict_pollen(img_path):
+    cls_names = []
+    key = 0
+
+    return cls_names[key]
